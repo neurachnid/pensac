29811b5b2c909baf7fa60d64b94798d87234137a
neurachnid <138285948+neurachnid@users.noreply.github.com>
2025-06-18 03:52:18 -0700

    Fix disposed variable error by preserving target weights

--- worker.js
+++ worker.js
@@ -419,7 +419,6 @@ class DDPGAgent {
             target.setWeights(newW);
             tf.dispose(tw);
             tf.dispose(sw);
-            tf.dispose(newW);
         };
         updateTarget(this.targetActor, this.actor);
         updateTarget(this.targetCritic, this.critic);
